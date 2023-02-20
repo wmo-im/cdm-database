1478e187b38f30ed3efb344dfa15977c53f454cc
david-i-berry <dberry@wmo.int>
2023-02-20 09:57:55 +0100

    Update following changes to pyopencdms.

--- build_database.py
+++ build_database.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 from opencdms.provider import opencdmsdb
 
+
 # set connection details
 UID = os.environ["POSTGRES_USER"]
 PWD = os.environ["POSTGRES_PASSWORD"]
@@ -12,7 +13,7 @@ DBNAME = os.environ["POSTGRES_DB"]
 DBHOST = os.environ["POSTGRES_HOST"]
 DBPORT = os.environ["POSTGRES_PORT"]
 
-engine = create_engine(f"postgresql+psycopg2://{UID}:{PWD}@{DBHOST}:{DBPORT}/{DBNAME}")
+engine = create_engine(f"postgresql+psycopg2://{UID}:{PWD}@{DBHOST}:{DBPORT}/{DBNAME}", echo=True)
 # check whether we want to start clean
 CLEAN = True
 if CLEAN:
@@ -38,28 +39,40 @@ code_tables = {
     "cdm.observation_type": "observation_type.csv",
     "cdm.observed_property": "observed_property.csv",
     "cdm.observing_procedure": "observing_procedure.csv",
-    "cdm.users": "users.csv",
+    "cdm.user": "users.csv",
     "cdm.record_status": "status.csv",
-    "cdm.hosts": "stations.csv",
+    "cdm.source_type": "source_type.csv",
     "cdm.source": "source.csv",
 }
-
+# note, order is important
 data_tables = {
-    "cdm.observations": ["CA_6016975_1953.csv"]
+    "cdm.host": ["hosts.csv"],
+    "cdm.observation": ["CA_6016527_1990.csv", "CA_6016527_1991.csv",
+                        "CA_6016527_1992.csv", "CA_6016527_1993.csv",
+                        "CA_6016527_1994.csv", "CA_6016527_1995.csv",
+                        "CA_6016527_1996.csv", "CA_6016527_1997.csv",
+                        "CA_6016527_1998.csv", "CA_6016527_1999.csv",
+                        "CA_6016527_2000.csv", "CA_6016527_2001.csv",
+                        "CA_6016527_2002.csv", "CA_6016527_2003.csv",
+                        "CA_6016527_2004.csv", "CA_6016527_2005.csv",
+                        "CA_6016527_2006.csv", "CA_6016527_2007.csv",
+                        "CA_6016527_2008.csv", "CA_6016527_2009.csv",
+                        "CA_6016527_2010.csv", "CA_6016527_2011.csv",
+                        "CA_6016527_2012.csv"]
 }
 
 for key, value in code_tables.items():
     with open(f"/local/app/data/code_tables/{value}") as fh:
-        cursor.copy_expert(f"COPY {key} FROM STDIN WITH CSV HEADER DELIMITER AS '|' NULL AS 'NA'", fh)
+        cursor.copy_expert(f"COPY {key} FROM STDIN WITH CSV HEADER DELIMITER AS '|' NULL AS 'NA' QUOTE E'\b'", fh)
 
 for key, value in data_tables.items():
     if isinstance(value, list):
         for item in value:
             with open(f"/local/app/data/data_tables/{item}") as fh:
-                cursor.copy_expert(f"COPY {key} FROM STDIN WITH CSV HEADER DELIMITER AS '|' NULL AS 'NA'", fh)
+                cursor.copy_expert(f"COPY {key} FROM STDIN WITH CSV HEADER DELIMITER AS '|' NULL AS 'NA' QUOTE E'\b'", fh)
     else:
         with open(f"/local/app/data/data_tables/{value}") as fh:
-            cursor.copy_expert(f"COPY {key} FROM STDIN WITH CSV HEADER DELIMITER AS '|' NULL AS 'NA'", fh)
+            cursor.copy_expert(f"COPY {key} FROM STDIN WITH CSV HEADER DELIMITER AS '|' NULL AS 'NA' QUOTE E'\b'", fh)
 
 session.commit()
 session.close()
